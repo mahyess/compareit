c1446bfe1dea9ecb94cd6b24a8c4a3dbc51579f2
ZephyRr <zephyrr2722@gmail.com>
2018-08-31 17:30:42 +0545

    test values in database

--- Desktop/FYP/PCE webapp/src/item/admin.py
+++ Desktop/FYP/PCE webapp/src/item/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Category, SubCategory, Item, ItemImage
+
+admin.site.register(Category)
+admin.site.register(SubCategory)
+admin.site.register(Item)
+admin.site.register(ItemImage)
