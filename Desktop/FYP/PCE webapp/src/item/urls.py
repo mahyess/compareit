0880509f9ca53872b390d678554b7e1faa083989
ZephyRr <zephyrr2722@gmail.com>
2018-09-05 16:01:31 +0545

    Image compatibility

--- Desktop/FYP/PCE webapp/src/item/urls.py
+++ Desktop/FYP/PCE webapp/src/item/urls.py
@@ -1,7 +1,9 @@
 
 from django.contrib import admin
 from django.urls import path, re_path, include
+from .views import (item)
 
 urlpatterns = [
-    # path('', home, name='home'),
+# path('', home, name='home'),
+     re_path(r'^(?P<id>\d+)/$',item, name='item'),
 ]
--- Desktop/FYP/PCE webapp/src/item/views.py
+++ Desktop/FYP/PCE webapp/src/item/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import (
+	Item,
+	ItemImage,
+)
 
 # Create your views here.
+def item(request, id):
+	item = get_object_or_404(Item, id=id)
+	itemImages = ItemImage.objects.filter(item=item)
+
+	context = {
+		'item' : item,
+		'title' : item.title,
+		'images' : itemImages
+	}
+	return render(request, 'item/item.html', context)
